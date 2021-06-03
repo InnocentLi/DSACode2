e132ec49eaad8de57457e6ffd1240d58a1fb32da
InnocentLiThinpadX1 <vincentlieie@gamil.com>
2021-06-03 08:06:34 +0800

    新增

--- ChallengeProgram挑战程序设计andMK/05HighClassSort/016_1归并排序mk版本.cpp
+++ ChallengeProgram挑战程序设计andMK/05HighClassSort/016_1归并排序mk版本.cpp
@@ -8,18 +8,43 @@
 
 // 递归 归并排序 对 arr[l...r]
 template<typename T>
-void __mergeSort(T arr[],int l,int r){
-    if(l>=r){
-        
+void __merge(T arr[],int l,int mid,int r){
+
+    T aux[r-l+1];
+    for(int i = l;i <= r;i++){
+        aux[i-l] = arr[i];
     }
+     int i = l,j = mid +1;
+     for(int k = l;k<=r;k++){
+         if(aux[i-l]<aux[j-1]){
+             arr[k] = aux[i-l];
+         }
+         else{
+             
+         }
+     }
+}
+
+
+template<typename T>
+void __mergeSort(T arr[],int l,int r){
+    if( l >= r ){
+        return;
+    }      
+    int mid = (l+r)/2;      // 溢出风险
+    __mergeSort(arr,l,mid);
+    __mergeSort(arr,mid+1,r);
+    __merge(arr,l,mid,r);
 }
 
  template<typename T>
  void mergeSort(T arr[],int n){
      //private
      __nergeSort(arr,0,n-1){
-
+        
      }
+    
+
  }
 
  int main(){
