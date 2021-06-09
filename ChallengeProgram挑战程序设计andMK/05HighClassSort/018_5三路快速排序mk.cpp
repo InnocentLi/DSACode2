d4cf04f251b7911e162da66b2bdef840b40eb7de
innocence <vincentlieie@gmail.com>
2021-06-09 17:01:01 +0800

    三路快速排序更新

--- ChallengeProgram挑战程序设计andMK/05HighClassSort/018_5三路快速排序mk.cpp
+++ ChallengeProgram挑战程序设计andMK/05HighClassSort/018_5三路快速排序mk.cpp
@@ -1,5 +1,5 @@
 // 三路快速排序
-
+// 
 // v   <v    =v     >v
 
 #include<iostream>
@@ -29,18 +29,35 @@ void __quickSOrt3Ways(T arr[],int l,int r){
         insertSort(arr,l,r);
         return;
     }
+    //不利用返回值  
     // partition 划分
     swap(arr[l],arr[rand()%(r-l+1)+l]);
     T v = arr[l];
     
-    int lt = l; // l+1......lt < v
+    int lt = l; // l+1......lt < v    
     int gt = r +1; // gt......r >v
-    
+    int i = l +1; // aer[lt+1.....i) ==v
+    while(i<gt){
+        if(arr[i]<v){
+            swap(arr[i],arr[lt+1]);
+            lt++;
+            i++;
+        }else if(arr[i]>v){
+            swap(arr[i],arr[gt-1]);
+            gt--;
+        }else{ //arr[i] == v;
+            i++;
+        }
+    }
+    swap(arr[l],arr[lt]);
+
+    __quickSOrt3Ways(arr,l,lt-1);
+    __quickSOrt3Ways(arr,gt,r);
 }
 
 // 快速排序三路版本
  template<typename T>
- void quickSort(T arr[],int n){
+ void quickSOrt3Ways(T arr[],int n){
      
     srand(time(NULL));
     __quickSOrt3Ways(arr,0,n-1);
@@ -49,5 +66,10 @@ void __quickSOrt3Ways(T arr[],int l,int r){
 
 
 int main(){
+    int n = 1000000;
+    int* arr = SortTestHelper::generateRandomArray(n,0,n);
+
+    SortTestHelper::testSort("三路快速排序",quickSOrt3Ways,arr,n);
+    delete[] arr;
 
 }
